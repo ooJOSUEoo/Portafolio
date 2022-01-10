2948850edf7dc7e4cae315863ecaab96c15d10bb
Josue <sjosue5082002@gmail.com>
2022-01-10 17:11:37 -0600

    implementacion de habilidades p2

--- js/habilidades.js
+++ js/habilidades.js
@@ -3,6 +3,10 @@ function setActiveClass(){
     list_h.forEach((item) => 
     item.classList.remove('habilidades__active-progress'));
     this.classList.add('habilidades__active-progress');
+
+    setTimeout(() => {
+        this.classList.remove('habilidades__active-progress');
+    }, 3000);
 }
 list_h.forEach((item) =>
 item.addEventListener('mouseover', setActiveClass));
